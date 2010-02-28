8398df4fbf000fdc3c43f3f621041da7ea90da19
User <tooru@moe.omakase.org>
2010-02-28 16:41:46 +0900

    initial commit

---
+++ lib/Plack/Middleware/MobileAgentLite.pm
@@ -0,0 +1,32 @@
+package Plack::Middleware::MobileAgentLite;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Plack::Middleware::MobileAgentLite -
+
+=head1 SYNOPSIS
+
+  use Plack::Middleware::MobileAgentLite;
+
+=head1 DESCRIPTION
+
+Plack::Middleware::MobileAgentLite is
+
+=head1 AUTHOR
+
+Tooru Midorikawa E<lt>tooru@omakase.orgE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
